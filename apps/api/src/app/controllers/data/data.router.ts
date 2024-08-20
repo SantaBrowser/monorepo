2357258c49bf73cbb8ba5569ae32b38fac2172f4
vijay <vijay@vijayakumar.co.in>
2024-08-20 11:33:48 +0530

    refresh issue

--- apps/api/src/app/controllers/data/data.router.ts
+++ apps/api/src/app/controllers/data/data.router.ts
@@ -7,13 +7,9 @@ import { ForbiddenError } from '@thxnetwork/api/util/errors';
 const router: express.Router = express.Router();
 
 const mixpanelProxy = function (options: AxiosRequestConfig) {
-  console.log('mixpanelProxy', options);
-  if (!options.url.startsWith('/')) throw new ForbiddenError();
+    if (!options.url.startsWith('/')) throw new ForbiddenError();
     axios.defaults.baseURL = MIXPANEL_API_URL;
-
-  console.log('mixpanelProxy', MIXPANEL_API_URL,options);
-
-  return axios(options);
+    return axios(options);
 };
 
 router.all('*', async (req: Request, res: Response) => {
@@ -27,13 +23,12 @@ router.all('*', async (req: Request, res: Response) => {
     //     const dataString = JSON.stringify(data);
     //     req.body.data = Buffer.from(dataString).toString('base64');
     // }
-    const response = await mixpanelProxy({
+    await mixpanelProxy({
         method: req.method as Method,
         url: req.originalUrl.replace(req.baseUrl, ''),
-        headers: { 'X-REAL-IP': '115.246.234.52' },
+        headers: { 'X-REAL-IP': req.ip },
         params: req.body,
     });
-    console.log(response);
     res.end();
 });
 
